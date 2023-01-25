6f6c04e15332c05874d9022a61d98fac7bef293c
Kate <KhovanskayaK@yandex.ru>
2023-01-25 10:26:34 +0300

    добавлена задача 41

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел строго больше 0 ввёл пользователь.
+Console.WriteLine("Введите любые целые числа через пробел");
+string[] numS = Console.ReadLine()!.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine($"Положительных чисел {NumPositive(numS)}");
+//----Методы------
+int NumPositive(string[] arrStr){
+    int quontPositive=0;
+    foreach(string str in arrStr){
+        if(Convert.ToInt32(str)>0){
+            quontPositive+=1;
+        }
+    }
+    return quontPositive;
+}
